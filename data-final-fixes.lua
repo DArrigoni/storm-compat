6df3efe50178246e071bea3f6944b06db4475f4d
Drew Arrigoni <arrigoni.andrew@gmail.com>
2017-03-27 09:25:14 -0400

    Unnecessary file

--- data-final-fixes.lua
+++
@@ -1,2 +0,0 @@
-data.raw["logistic-robot"]["logistic-robot"].minable=asdf
-data.raw["construction-robot"]["construction-robot"].minable= {hardness = 0.1, mining_time = 0.1, result = "construction-robot"}
